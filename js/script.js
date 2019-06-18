ca9cfb06f24e19121cdcc1994978159ffd9978a7
nacp <44129347+nacp@users.noreply.github.com>
2019-06-18 20:12:13 -0300

    Update 2019-06-18
    Agregado el conteo de clientes.

--- js/script.js
+++ js/script.js
@@ -13,36 +13,51 @@ function display_none() { // Función que oculta todos los días.
   document.getElementById("ABJueves").style.display="none";
   document.getElementById("ABViernes").style.display="none";
   document.getElementById("ABSabado").style.display="none";
+  document.getElementById("contador").style.display="none";
 
 };
 
-function Ruta2(dias) { //Función que devuelve la ruta del día.
+function Ruta_dia(dia) { //Función que devuelve la ruta del día.
 
-  switch (dias) {
+  contador=document.getElementById("cli_tot");
+  document.getElementById("contador").style.display="block";
+
+  switch (dia) {
     case 0:
       document.getElementById("Domingo").style.display="block";
+      document.getElementById("contador").style.display="none";
       break;
     case 1:
       document.getElementById("Lunes").style.display="block";
+      contador.innerHTML=28;
       break;
 	 case 2:
       document.getElementById("Martes").style.display="block";
+      contador.innerHTML=35;
       break;
-    case 3: document.getElementById("Miercoles").style.display="block";
+    case 3:
+      document.getElementById("Miercoles").style.display="block";
+      contador.innerHTML=36;
       break;
-    case 4: document.getElementById("Jueves").style.display="block";
+    case 4:
+      document.getElementById("Jueves").style.display="block";
+      contador.innerHTML=29;
       break;
-    case 5: document.getElementById("Viernes").style.display="block";
+    case 5:
+       document.getElementById("Viernes").style.display="block";
+       contador.innerHTML=37;
       break;
-    case 6: document.getElementById("Sabado").style.display="block";
+    case 6:
+       document.getElementById("Sabado").style.display="block";
+       contador.innerHTML=15;
       break;
   };
 
 };
 
-function RutaABC(dias) { //Función que devuelve la ruta del día ordenada alfabétiamente.
+function RutaABC(dia) { //Función que devuelve la ruta del día ordenada alfabétiamente.
 
-  switch (dias) {
+  switch (dia) {
     case 0:
       document.getElementById("Domingo").style.display="block";
       break;
@@ -68,33 +83,67 @@ function RutaABC(dias) { //Función que devuelve la ruta del día ordenada alfab
 
 };
 
-function ruta_hoy() {
-
+function ruta_hoy(Primero) { /* Primero indica si es la primera vez que
+                                se ejecuta la función. */
   var hoy, lista;
 
   //Obtengo el día de hoy
   hoy = new Date();
+
+  dia = hoy.getDay();
     
   lista = document.getElementById("dias")
-  lista.selectedIndex = hoy.getDay();
-    
+  lista.selectedIndex = dia;
+
+  sessionStorage.setItem("C_Hoy", 0);
+
+  if (Primero) {
+    switch (dia) {
+      case 1:
+        sessionStorage.setItem("C_Hoy", 28);
+        break;
+      case 2:
+        sessionStorage.setItem("C_Hoy", 35);
+        break;
+      case 3:
+        sessionStorage.setItem("C_Hoy", 36);
+        break;
+      case 4:
+        sessionStorage.setItem("C_Hoy", 29);
+        break;
+      case 5:Ussel 
+        sessionStorage.setItem("C_Hoy", 37);
+        break;
+      case 6:
+        sessionStorage.setItem("C_Hoy", 15);
+        break;
+    }
+    document.getElementById("cli_quedan").innerHTML=sessionStorage.C_Hoy+"/";
+  };
+
   ruta();
 
 };
 
 function ruta() {
 
-  var dia, lista, abc;
+  var dia, lista, abc, cont_hoy, hoy;
 
   display_none();
 
+  hoy = new Date();
+  cont_hoy = document.getElementById("cli_quedan");
+
   //Obtengo la opción seleccionada      
   lista = document.getElementById("dias");
-  dia = lista.options[lista.selectedIndex].value;
+  dia = lista.selectedIndex;
 
   abc = document.getElementById("ABC").checked
 
-  if (!abc) { Ruta2(Number(dia)) }
+  if ( dia != hoy.getDay()) { cont_hoy.style.display="none"}
+  else { cont_hoy.style.display="inline"};
+
+  if (!abc) { Ruta_dia(Number(dia)) }
   else { RutaABC(Number(dia)) };
 
 }  // Fin ruta.
@@ -128,5 +177,17 @@ function dia_sig() {
 }
 
 function Eliminar(AA){ 
+  var hoy, lista, abc, cliente, visitados;
+
+  lista = document.getElementById("dias");
+  hoy = new Date();
+  dia = lista.selectedIndex;
+  abc = document.getElementById("ABC").checked;
+  if (dia == hoy.getDay()) {
+    if (!abc) {
+      sessionStorage.C_Hoy = Number(sessionStorage.C_Hoy) - 1;
+      document.getElementById("cli_quedan").innerHTML = sessionStorage.C_Hoy + "/";
+    };
    AA.parentElement.style.display = "none";
+  };
 }
